2f99ee6b40e33b725424858e104bff84e9919c2d
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-08-26 12:35:41 +0530

    Add files via upload

---
+++ oops/Polymorphism.cpp
@@ -0,0 +1,39 @@
+//Polymorphism = poly means many/ various types // morphism means forms 
+// overall it means that many types of forms basically different ways
+// it has two types 1. Compile time polymorphism , 2. Run time polymorphism
+// if i wanted to do function overloading then i  have to change input arguments.
+
+//function overloading 
+
+// #include<iostream>
+// using namespace std;
+
+// class A {
+//   public:
+//    void display(){
+//      cout<< "The value of first display is 2" << endl;
+    
+//    }
+
+
+//    int display(int age){
+//     cout<< "The value of second number is 3" << endl;
+//     return 1;
+//    }
+
+//    void display(char name){
+//     cout<< "THe value of third number is 3" << endl;
+    
+//    }
+// };
+
+// int main(){
+
+//  A obj;
+//    obj.display();
+//   return 0;
+// }
+
+//ABSTRACTION = Impleamentation hiding , just showing only nessescary features of the class.
+
+
---
+++ oops/oops.cpp
@@ -0,0 +1,91 @@
+//Encapsulation:
+
+// #include<iostream>
+// using namespace std;
+
+// class Student{
+//   private:
+//    string name;
+//    int age;
+//    int rollno;
+
+
+//    public:
+//     int getRollno() {
+//       return this -> rollno;
+//     }
+// };
+
+
+// int main(){
+
+//    Student s1;
+//    cout << "student s1s2" << endl;
+
+// }
+
+// Inheritance:
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Human{
+  public: 
+   int age;
+   int height;
+   int weight;
+
+
+   public: 
+    int getAge(){
+      return this -> age;
+    }
+
+     int  getHeight(){
+      return this -> height;
+     }
+
+     int getWeight(){
+      return this -> weight;
+     }
+};
+
+// if any attribute of parent class is in private  and  we want that attribute to be in child class as a public then that attribute wan't be acssesible in child class (public)
+   
+    
+
+    class Male : public Human {
+      public: 
+       string color;
+
+       void sleep(){
+         cout << "Male are sleeping"<< endl;
+       }
+    
+};
+
+
+int main(){
+    Male object1;
+
+    object1.age = 4;
+    object1.height = 5;
+    object1.color =  "red";
+    object1.weight = 6;
+
+
+    cout<< object1.age << endl;
+    cout << object1.height << endl;
+    cout<< object1.color << endl;
+    cout << object1.weight << endl;
+    return 0;
+}
+
+
+// Inheritance have some types :
+// 1. Single level inheritance = normal inheritance
+// 2. Multilevel inheritance = one parent class through which child1 is inherited and then chlid2 inherited from child1.
+// 3. Multiple inheritance = one parent class then another class and then  a new class want to inheritedf from both of the classes before
+// 4. Hierarchial inheritance = two child class has their one commen parent class to inheritance
+// 5. Hybrid Inheritance = combination of more than two types of inheritance
