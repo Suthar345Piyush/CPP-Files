b19f2ab3c0c26ba0cc46b7383bb72f1f5da6a40e
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-08-28 18:26:26 +0530

    Update SmartPointers.cpp

--- SmartPointers.cpp
+++ SmartPointers.cpp
@@ -1,53 +1,53 @@
 // Smart pointers are use to less the memory leaks and security issues made by pointers :
 //1. Unique pointers = it freeze up the memory as the scope ends, we cannot share the memory references and a multiple refrences are not point to same location.
 
-// #include<iostream>
-// #include<string>
-// #include<memory>
-// using namespace std;
+#include<iostream>
+#include<string>
+#include<memory>
+using namespace std;
 
-// class User{
-//   public:
-//   //constructor
+class User{
+  public:
+  //constructor
 
-//      User(){
-//       cout << "User created\n";
-//      }
-//  //destructor
+     User(){
+      cout << "User created\n";
+     }
+ //destructor
 
-//      ~User(){
-//       cout << "User destroyed\n";
-//      }
+     ~User(){
+      cout << "User destroyed\n";
+     }
 
-//       void testFunc(){
-//         cout << "Testing the function" << endl;
-//       }
-// };
+      void testFunc(){
+        cout << "Testing the function" << endl;
+      }
+};
 
-// int main(){
-// {
+int main(){
+{
   
  
 
 
-//   unique_ptr<User> piyush = make_unique<User>();
-//   piyush -> testFunc();
-// }
+  unique_ptr<User> piyush = make_unique<User>();
+  piyush -> testFunc();
+}
 
 // // we have another smart pointer called shared pointer the syntax is same as unique pointer, we can use it to  share memory refrences to multiple pointers 
 
-// {
-//    shared_ptr<User> Suthar = make_shared<User>();
-//    shared_ptr<User> Sutharr = Suthar;
+{
+   shared_ptr<User> Suthar = make_shared<User>();
+   shared_ptr<User> Sutharr = Suthar;
 
-//    cout << "It can share memory refrences" << endl;
-// }
+   cout << "It can share memory refrences" << endl;
+}
 
-//   // unique_ptr<User> piyushh =  piyush ; // this type syntax is not allowed 
+  // unique_ptr<User> piyushh =  piyush ; // this type syntax is not allowed 
      
-//      cout<< "program ended" << endl;
-//   return 0;
-// }
+     cout<< "program ended" << endl;
+  return 0;
+}
 
 // swap two number program
 //with move semantics: 
