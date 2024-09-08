f88c670a4892dbdb69a60a01298b66f83c8822ec
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-09-08 09:29:43 +0530

    Add files via upload

--- CoreSTL/Constructors/Stack.cpp
+++ CoreSTL/Constructors/Stack.cpp
@@ -1,162 +1,183 @@
 //stack follows firstin lastout
 
-#include<iostream>
-#include<stack>
-using namespace std;
+// #include<iostream>
+// #include<stack>
+// using namespace std;
 
-int main(){
-  stack<int> s;
-  s.push(1);
-  s.push(2);
-  s.push(3);
-  s.push(4);
-  s.push(5);
+// int main(){
+//   stack<int> s;
+//   s.push(1);
+//   s.push(2);
+//   s.push(3);
+//   s.push(4);
+//   s.push(5);
 
-  int num = 0;
-  s.push(num);
-  s.pop();
-  s.pop();
-  s.pop();
-  s.pop();
+//   int num = 0;
+//   s.push(num);
+//   s.pop();
+//   s.pop();
+//   s.pop();
+//   s.pop();
 
-  while(!s.empty()){
-    cout << s.top() << " ";
-    s.pop();
-  } // if stack is empty, loop ends.
-    // if stack not empty , loop continue.
-}
+//   while(!s.empty()){
+//     cout << s.top() << " ";
+//     s.pop();
+//   } // if stack is empty, loop ends.
+//     // if stack not empty , loop continue.
+// }
 
 
-#include<iostream>
-#include<stack>
-using namespace std;
-int main(){
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// int main(){
 
-  stack<int> stacks;
-  stacks.push(5);
-  stacks.push(6);
-  cout << stacks.top() << endl;
+//   stack<int> stacks;
+//   stacks.push(5);
+//   stacks.push(6);
+//   cout << stacks.top() << endl;
 
-}
+// }
 
 //stack sum operation.
-#include<iostream>
-#include<stack>
-using namespace std;
+// #include<iostream>
+// #include<stack>
+// using namespace std;
 
-int main(){
+// int main(){
 
-  stack<int> allStacks;
-  int sum  = 0;
-  allStacks.push(5);
-  allStacks.push(6);
-  allStacks.push(7);
-  allStacks.push(9);
+//   stack<int> allStacks;
+//   int sum  = 0;
+//   allStacks.push(5);
+//   allStacks.push(6);
+//   allStacks.push(7);
+//   allStacks.push(9);
 
 
-//it returns true if empty otherwise loop continues
-  while(!allStacks.empty()){
-    sum += allStacks.top();
-    allStacks.pop();
-  }
-  cout << sum << endl;
+// //it returns true if empty otherwise loop continues
+//   while(!allStacks.empty()){
+//     sum += allStacks.top();
+//     allStacks.pop();
+//   }
+//   cout << sum << endl;
 
-}
+// }
 
 //empty() and size() in stack;
 
-#include<iostream>
-#include<stack>
-using namespace std;
+// #include<iostream>
+// #include<stack>
+// using namespace std;
 
-int main(){
-  stack<int> myStacks;
+// int main(){
+//   stack<int> myStacks;
 
-  myStacks.push(3);
-  myStacks.push(4);
-  myStacks.push(5);
+//   myStacks.push(3);
+//   myStacks.push(4);
+//   myStacks.push(5);
 
-  if(myStacks.empty()){
-    cout << "My stack is empty" << endl;
+//   if(myStacks.empty()){
+//     cout << "My stack is empty" << endl;
 
-  }
-  else{
-    cout << "My satck is full" << endl;
+//   }
+//   else{
+//     cout << "My satck is full" << endl;
 
-  }
-}
+//   }
+// }
 
 //size() operations:
-#include<iostream>
-#include<stack>
-using namespace std;
+// #include<iostream>
+// #include<stack>
+// using namespace std;
 
-int main(){
-  stack<int> myStacks;
+// int main(){
+//   stack<int> myStacks;
 
-  myStacks.push(4);
-  myStacks.push(5);
-  myStacks.push(2);
-  myStacks.push(1);
-        cout <<  myStacks.size() << endl;
-}
+//   myStacks.push(4);
+//   myStacks.push(5);
+//   myStacks.push(2);
+//   myStacks.push(1);
+//         cout <<  myStacks.size() << endl;
+// }
 
-#include<iostream>
-#include<Stack>
-using namespace std;
+// #include<iostream>
+// #include<Stack>
+// using namespace std;
 
-int main(){
+// int main(){
 
-  stack<int> stacks;
-  int sum = 0;
-  stacks.push(6);
-  stacks.push(9);
-  stacks.push(4);
-  stacks.push(2);
+//   stack<int> stacks;
+//   int sum = 0;
+//   stacks.push(6);
+//   stacks.push(9);
+//   stacks.push(4);
+//   stacks.push(2);
 
- while(stacks.size() > 0){
-sum = sum + stacks.top();
- stacks.pop();
+//  while(stacks.size() > 0){
+// sum = sum + stacks.top();
+//  stacks.pop();
+//  cout << sum << endl;
+//  }
+// }
 
+// #include<iostream>
+// #include<stack>
+// using namespace std;
 
- cout << sum << endl;
- }
-}
+// int main(){
+
+//   int s = 0;
+//   stack<int> isStack;
+
+//   isStack.push(6);
+//   isStack.push(8);
+//   isStack.push(7);
+ 
+//   while(!isStack.empty()){
+//     isStack.pop();
+//     s++;
+//   }
+
+//   cout << s << endl;
+
+// }
 
 //swaping in stack
 
-#include<iostream>
-#include<stack>
-using namespace std;
+// #include<iostream>
+// #include<stack>
+// using namespace std;
 
-int main(){
+// int main(){
 
-  stack<int> A1;
-  stack<int> A2;
+//   stack<int> A1;
+//   stack<int> A2;
 
-  A1.push(7);
-  A1.push(5);
-  A1.push(8);
+//   A1.push(7);
+//   A1.push(5);
+//   A1.push(8);
 
-  A2.push(2);
-  A2.push(3);
-  A2.push(9);
+//   A2.push(2);
+//   A2.push(3);
+//   A2.push(9);
 
-  A1.swap(A2);
+//   A1.swap(A2);
 
-  cout << "A1= ";
-  while(!A1.empty()){
-    cout << A1.top() <<endl;
-    A1.pop();
-  }
+//   cout << "A1= ";
+//   while(!A1.empty()){
+//     cout << A1.top() <<endl;
+//     A1.pop();
+//   }
 
-  cout << endl<< "A2 = ";
-  while(!A2.empty()){
-    cout << A2.top() << endl;
-    A2.pop();
-  }
+//   cout << endl<< "A2 = ";
+//   while(!A2.empty()){
+//     cout << A2.top() << endl;
+//     A2.pop();
+//   }
    
-}
+// }
+
 
 //emplace() it add new value on the top of the top of stack
 
@@ -184,7 +205,3 @@ int main(){
 
 
 }
-
-
-
-
