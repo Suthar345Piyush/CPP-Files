2b05f50bd0b0b0d15ea8e0faaa8c7c29e6c25319
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-09-12 18:51:33 +0530

    Update ForwardIterator.cpp

--- CoreSTL/Iterators/ForwardIterator.cpp
+++ CoreSTL/Iterators/ForwardIterator.cpp
@@ -20,3 +20,48 @@ int main(){
   }
   
 }
+
+
+//bidirectional iterators:
+// they move in both directions, they can be decremented
+
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main(){
+  list<int> l1 = {1, 3, 4, 5,6, 7 ,8};
+
+  list<int> :: iterator it;
+
+//decrementing from last element
+  for(it = l1.end(); it != l1.begin(); it--){
+  if(it != l1.end()){
+  cout << (*it) << " ";
+  }
+ }
+ cout << *it << " ";
+}
+
+
+
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main(){
+
+  list<int> l1 = {1, 2, 4 ,56, 77};
+
+  list<int> :: iterator it1;
+
+  for(it1 = l1.end(); it1 != l1.begin(); --it1){
+    cout << (*it1) << " ";
+  }
+
+  // for(it1 = l1.end(); it1 != l1.begin(); --it1){
+  //   cout << (*it1) << " ";
+  // }
+
+  cout << endl;
+}
