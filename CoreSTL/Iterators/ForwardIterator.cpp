18d6a2a4b15f393754cf49475e4769da1995cd3c
Piyush Suthar <Piyushsuthar524@gmail.com>
2024-09-12 19:24:49 +0530

    Update ForwardIterator.cpp

--- CoreSTL/Iterators/ForwardIterator.cpp
+++ CoreSTL/Iterators/ForwardIterator.cpp
@@ -65,3 +65,58 @@ int main(){
 
   cout << endl;
 }
+
+
+
+//Random Acsess iterators:
+//it can be incremented as well as decremented
+//it works with vectors,in this we relational and arithmatic operators
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+  vector<int> v1= {1,2, 3, 4 ,5 ,6  };
+
+  vector<int> :: iterator itr1;
+
+  for(itr1 = v1.end() -  1; itr1 != v1.begin() - 1; --itr1 ){
+     *itr1 = 8;
+  } 
+  for(itr1 = v1.end() - 1 itr1 != v1.begin() - 1; --itr1){
+    cout <<  *itr1 << " ";
+  }
+  cout << endl;
+}
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+
+  vector<int> v1 = {1, 2, 3}; 
+
+  vector<int> :: iterator it1;
+  vector<int> :: iterator it2;
+
+
+  it1 = v1.begin();
+  it2 = v1.end();
+
+  if(it1 > it2){
+    cout << "Never" << endl;
+  }
+
+  int count = it2 - it1; // this is irrespective of value
+  cout << count <<endl;
+
+
+}
+
